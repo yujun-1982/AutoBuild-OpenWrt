c16ec9c9d772b30384297edd433c27c8f844ae8d
yujun-1982 <80206815+yujun-1982@users.noreply.github.com>
2021-03-19 20:20:38 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,12 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/10.10.10.2/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. 修改主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWRT'' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#3. 设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' openv/rt/package/lean/default-settings/files/zzz-default-settings
+
+#4. 版本号里显示一个自己的名字
+sed -i "s/OpenWrt /Ysian build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
