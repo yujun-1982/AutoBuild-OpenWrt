a57c6d959c4e40e1e9979be5f09d61b13fc7aed3
yujun-1982 <80206815+yujun-1982@users.noreply.github.com>
2024-11-14 06:01:10 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -14,3 +14,8 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
 
+#4. 修改主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWRT'' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#5. .修改版本号
+sed -i "s/OpenWrt /Lunseil build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
